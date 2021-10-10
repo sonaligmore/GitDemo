16365526b5fdc7ff887b467968b858e1ef1880f1
sonali <sonaligmore@gmail.com>
2021-10-10 17:19:32 -0400

    This is commit from EstTimezone

--- src/test/java/qaclickacademy/RestassuredAPITest.java
+++ src/test/java/qaclickacademy/RestassuredAPITest.java
@@ -6,12 +6,17 @@ public class RestassuredAPITest {
 	@Test
 	public void postjira()
 	{
-		System.out.println("postjira");
+		System.out.println("postjira2");
+		
 	}
 	@Test
 	public void deleteTwitter()
 	{
 		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter1");
+
+		System.out.println("deleteTwitter2");
+
 
 	}
 
